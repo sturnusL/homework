de37dd4df4b5f1aa6ae23f077f1b20360eb95c7f
sturnusL <91370723+sturnusL@users.noreply.github.com>
2021-09-25 18:01:05 +0800

    Add files via upload

---
+++ homework1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+char main()
+{
+    
+    printf(" %c\n",96);
+    return 0;
+}
---
+++ homework2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" Startled by the sudsden sound,Sally shouted, 'By the Great Pumpkin,what is it!'\a") ;
+    
+     return 0;
+
+}
